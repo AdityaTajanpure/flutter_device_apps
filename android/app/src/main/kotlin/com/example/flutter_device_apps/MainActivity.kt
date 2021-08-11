49b434f097ca13978d5463126d7f7850bd1a1620
Aditya Tajanpure <67155550+AdityaTajanpure@users.noreply.github.com>
2021-08-11 16:01:06 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_device_apps/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.flutter_device_apps
+
+
+import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivityLaunchConfigs.BackgroundMode.transparent
+
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        intent.putExtra("background_mode", transparent.toString())
+        super.onCreate(savedInstanceState)
+    }
+}
